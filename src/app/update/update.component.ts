1a6ae05a9bf7c1c1c619cf2e89cdf42521db953f
Mike De Groot <mdegroot09@gmail.com>
2019-07-02 22:09:36 -0600

    added Update inputs and inserted existing values

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {User} from '../models/user';
 import {Observable} from 'rxjs';
-import {select, Store} from '@ngrx/store';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'app-update',
@@ -15,15 +15,17 @@ export class UpdateComponent {
   users: Observable<User[]>;
   userIndex
   user = {}
-  // user = {firstName: 'mike', lastName: 'de groot', email: 'mike@email.com'}
   
   constructor(private store: Store<{ users: User[] }>, private router: Router) {
-    // this.users = store.pipe(select('users'));
-
     // grab redux index of user off params -- has to be easier way
     this.userIndex = +router.getCurrentNavigation().finalUrl.root.children.primary.segments[1].path
-    // pull unique user directly off of store
+    // pull unique user directly off store -- better way?
     this.user = store.source['_value'].users[this.userIndex]
     console.log('this.user:', this.user)
+  }
+
+  show = (firstName, lastName, email) => {
+    event.preventDefault()
+    console.log(firstName, lastName, email)
   }
 }
